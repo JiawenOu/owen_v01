07f42eb6df861bfbfc1f0ead4bacc7cc8ca88471
OOOOwen <JiawenOu@dhcp-10-105-50-49.wireless.northwestern.private>
2016-04-20 14:38:52 -0500

    rooms

--- auto_dropdown_item.js
+++ auto_dropdown_item.js
@@ -15,4 +15,21 @@ test.controller('MyController', function($scope) {
             	Id: 4,
             	Name: 'Strawberry'
             }];
+	$scope.rooms = [{
+					Id: 1,
+					Name: 'One',
+					Num: 'rn1',
+				},  {
+					Id: 2,
+					Name: 'Two',
+					Num: 'rn2',
+				},  {
+					Id: 3,
+					Name: 'Three',
+					Num: 'rn3',
+				}, 	{
+					Id: 4,
+					Name: 'Four',
+					Num: 'rn4',
+				}];
 });
