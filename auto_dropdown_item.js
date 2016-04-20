33d1777f113e63103fcb7f39d34a3b6baba78ebf
OOOOwen <JiawenOu@ouyoshifumis-MacBook-Pro.local>
2016-04-20 18:35:46 -0500

    CSS improvement

--- auto_dropdown_item.js
+++ auto_dropdown_item.js
@@ -8,39 +8,39 @@ test.controller('MyController', function($scope) {
 				kinect: '',
 			};
 	$scope.buildings = [{
-				Id: ' #1',
+				Id: ': #1',
 				Name: 'One',
 			},  {
-				Id: ' #2',
+				Id: ': #2',
 				Name: 'Two',
 			},  {
-				Id: ' #3',
+				Id: ': #3',
 				Name: 'Three',
 			}, 	{
-				Id: ' #4',
+				Id: ': #4',
 				Name: 'Four',
 			}];
 	$scope.rooms = [{
-				Id: ' #1',
+				Id: ': #1',
 				Name: 'One',
 			},  {
-				Id: ' #2',
+				Id: ': #2',
 				Name: 'Two',
 			}, 	{
-				Id: ' #3',
+				Id: ': #3',
 				Name: 'Three',
 			}];
 	$scope.kinects = [{
-				Id: ' #1',
+				Id: ': #1',
 				Name: 'One',
 			},  {
-				Id: ' #2',
+				Id: ': #2',
 				Name: 'Two',
 			},  {
-				Id: ' #3',
+				Id: ': #3',
 				Name: 'Three',
 			}, 	{
-				Id: ' #4',
+				Id: ': #4',
 				Name: 'Four',
 			}];
 });
--- myJS.js
+++ myJS.js
@@ -1,5 +1,10 @@
 
 
-$('input[name=rn]').click(function() {
-  $('#room').html("Romm: #" + $('input[name=rn]:checked').val()+  "<i class='mdi-navigation-arrow-drop-down'></i>")
+$(document).ready(function(){
+  $("#demo").on("hide.bs.collapse", function(){
+    $(".btn").html('<span class="glyphicon glyphicon-collapse-down"></span> Open');
+  });
+  $("#demo").on("show.bs.collapse", function(){
+    $(".btn").html('<span class="glyphicon glyphicon-collapse-up"></span> Close');
+  });
 });
