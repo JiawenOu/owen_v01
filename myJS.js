7490ee0727389d89959786f2f4440dd151f31333
OOOOwen <JiawenOu@ouyoshifumis-MacBook-Pro.local>
2016-04-20 21:52:42 -0500

    UI improvement, add the search button

--- myJS.js
+++ myJS.js
@@ -1,10 +1,2 @@
 
 
-$(document).ready(function(){
-  $("#demo").on("hide.bs.collapse", function(){
-    $(".btn").html('<span class="glyphicon glyphicon-collapse-down"></span> Open');
-  });
-  $("#demo").on("show.bs.collapse", function(){
-    $(".btn").html('<span class="glyphicon glyphicon-collapse-up"></span> Close');
-  });
-});
